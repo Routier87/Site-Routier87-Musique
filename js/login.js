fdbca6466376599f2297c73ca18ca7f4400ffdd6
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-06-02 23:29:09 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,26 +1,65 @@
-function login(){
-
-    const user =
-        document.getElementById("user").value;
-
-    const pass =
-        document.getElementById("pass").value;
-
-    if(user === "demo" && pass === "1234"){
-
-        localStorage.setItem(
-            "connected",
-            "true"
-        );
-
-        window.location.href =
-            "dashboard.html";
-
-    }else{
-
-        alert(
-            "Identifiant ou mot de passe incorrect"
-        );
-
+const users = [
+    {
+        username: "Samantha",
+        password: "2026"
+    },
+    {
+        username: "Océane",
+        password: "2026"
+    },
+    {
+        username: "Chicco",
+        password: "2026"
+    },
+    {
+        username: "Christian",
+        password: "2026"
+    },
+    {
+        username: "Routier87",
+        password: "878787",
+        role: "admin"
     }
+];
+
+function login() {
+
+    const username = document
+        .getElementById("user")
+        .value
+        .trim();
+
+    const password = document
+        .getElementById("pass")
+        .value
+        .trim();
+
+    const user = users.find(
+        u =>
+        u.username === username &&
+        u.password === password
+    );
+
+    if (!user) {
+        alert("Identifiant ou mot de passe incorrect");
+        return;
+    }
+
+    localStorage.setItem(
+        "connected",
+        "true"
+    );
+
+    localStorage.setItem(
+        "username",
+        user.username
+    );
+
+    localStorage.setItem(
+        "role",
+        user.role || "user"
+    );
+
+    window.location.href =
+        "dashboard.html";
 }
